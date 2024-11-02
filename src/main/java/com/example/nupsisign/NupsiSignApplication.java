31f912b4df5afc1319417d832f74a8601418c823
VictorMS-200 <victorm20005@gmail.com>
2024-11-02 17:29:03 -0300

    Inicializando projeto

---
+++ src/main/java/com/example/nupsisign/NupsiSignApplication.java
@@ -0,0 +1,13 @@
+package com.example.nupsisign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NupsiSignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NupsiSignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nupsisign/NupsiSignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nupsisign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NupsiSignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
